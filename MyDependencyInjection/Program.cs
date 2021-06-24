1712a830f0e65c8a7bffdd0244d0e64ac1ab0ead
Kakaranish <stasiekgruz@gmail.com>
2021-06-24 19:04:04 +0200

    Add project files.

---
+++ MyDependencyInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
